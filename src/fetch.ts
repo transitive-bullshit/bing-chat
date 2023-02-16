2a6f92760ca6068195ae145cc50cabde6325141a
cyrusnaficy <cyrus.naficy@gmail.com>
2023-02-16 16:42:52 -0500

    PR changes (comments)

--- src/fetch.ts
+++
@@ -1,9 +0,0 @@
-/// <reference lib="dom" />
-
-const fetch = globalThis.fetch
-
-if (typeof fetch !== 'function') {
-  throw new Error('Invalid environment: global fetch not defined')
-}
-
-export { fetch }
